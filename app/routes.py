052637a87e29d123873742ad55a1cb5792ceda1c
marzort <maria.ortiz559@myci.csuci.edu>
2026-08-03 13:14:59 -0700

    updated login view function

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -20,7 +20,16 @@ def index():
 # render_template() takes a template filename and a variable list of template arguments
 # and returns the same template, but with all the placeholders in it replaced with actual values
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST']) # override default methods accepted
 def login():
     form = LoginForm()
+    # processing work
+    # GET --> False --> render template
+    # POST --> gather all data and run all validators --> if all good, return True
+    if form.validate_on_submit():
+        # flash() will show a message to the user to confirm input
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        # redirect() tells client's web browser to auto navigate to different page
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
