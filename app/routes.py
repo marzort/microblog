014e6fd5a9b0227c6b45129c8a1e2e7a9de0e2f5
marzort <maria.ortiz559@myci.csuci.edu>
2026-08-03 13:00:36 -0700

    made login form accessible via url

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -18,3 +19,8 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)
 # render_template() takes a template filename and a variable list of template arguments
 # and returns the same template, but with all the placeholders in it replaced with actual values
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
