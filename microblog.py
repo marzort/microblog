2bf72e5a60259a8fd140332e33c120d1d31319ea
marzort <maria.ortiz559@myci.csuci.edu>
2026-08-17 13:40:29 -0700

    finished chapter 4

--- microblog.py
+++ microblog.py
@@ -1,2 +1,10 @@
-from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
 # imports the app variable that is a member of the app package
+from app import app, db
+from app.models import User, Post
+
+# decorator that registers the function as a shell context function
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
