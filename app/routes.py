178ec04f4114677be63a280fd043016fa110d404
marzort <maria.ortiz559@myci.csuci.edu>
2026-08-17 14:11:10 -0700

    updated route for login

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from flask import render_template, flash, redirect, url_for
-from app import app
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import app, db
 from app.forms import LoginForm
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -22,14 +25,18 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST']) # override default methods accepted
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     # processing work
     # GET --> False --> render template
     # POST --> gather all data and run all validators --> if all good, return True
     if form.validate_on_submit():
-        # flash() will show a message to the user to confirm input
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        # redirect() tells client's web browser to auto navigate to different page
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
